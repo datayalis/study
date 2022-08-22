abc21545f072c4dd5ee0567ad52afbc8b061aa8c
datayalis <105771202+datayalis@users.noreply.github.com>
2022-08-22 06:29:03 -0400

    Add files via upload

---
+++ lectures.py
@@ -0,0 +1,49 @@
+#def thing():
+#    print('Hello')
+#    print('Fun')
+#thing()
+#print('Zip')
+#thing()
+big = max('yalisaleman')
+print(big)
+tiny= min('yalisaleman')
+print(tiny)
+
+#Type convestions
+print (float(99) / 100)
+i = 42
+type(i)
+f = float(i)
+
+# Check the Python Version
+
+import sys
+print(sys.version)
+
+# Loops
+n=5
+while n > 0 :
+    print(n)
+    n = n - 1
+print(n)
+print('Blastoff!')
+
+# REPEATED LOOP
+#y=5
+#while y > o :
+#    print('Lather')
+#    print ('Rinse')
+#print('Dry off!')
+
+B = 0
+while True:
+    if B == 3:
+        break
+    print(B)
+    B = B + 1
+
+ # definite loop w/ strings
+# friends = ['Matthew', 'Luke', 'John']
+# for friend in friends :
+#     print('Happy New Year:', friend)
+# print('Done!')
